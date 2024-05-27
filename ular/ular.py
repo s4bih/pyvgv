a8e73a2359a3c4ec4a5eb4249afa2964fc834257
s4bih <msabihamin@gmail.com>
2024-05-27 21:24:20 +0800

    halo

--- ular/ular.py
+++ ular/ular.py
@@ -1,5 +1,7 @@
 import pygame
 import time
+import random
+import math
 
 pygame.init()
 
@@ -16,10 +18,10 @@ pygame.display.set_caption(window_title)
 
 #snake class to move the snake and growth
 class Snake:
-    def __init__(self):
+    def __init__(self,speed):
         self.body=[(width//2-i*20,hight//2) for i in range(4)]
         self.direction=(1,0)
-
+        self.speed=speed
     def move(self):
         dx,dy=self.direction
         new_head=(self.body[0][0]+20*dx,self.body[0][1]+20*dy)
@@ -41,11 +43,68 @@ class Snake:
     def get_body(self):
         return self.body[1:]
 
+    def Speed(self,speed):
+        self.speed=speed
+
+class Food:
+    def __init__(self):
+        self.pos=(random.randint(10,width-10),random.randint(10,hight-10))
+
+    def respawn(self):
+        self.pos = (random.randint(10, width - 10), random.randint(10, hight - 10))
+
+    def get_position(self):
+        return self.pos
+
+def distance(pos1,pos2):
+    return math.sqrt((pos1[0]-pos2[0])**2+(pos1[1]-pos2[1])**2)
+
+def intro():
+    font_l=pygame.font.Font(None,50)
+    font_s=pygame.font.Font(None,30)
+
+    game_n=font_l.render("SNAKE GAME",True,black)
+    slow_b=font_s.render("SLOW",True,black)
+    normal_b=font_s.render("NORMAL",True,black)
+    fast_b=font_s.render("FAST",True,black)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+
+            if event.type==pygame.MOUSEBUTTONDOWN:
+                x,y=pygame.mouse.get_pos()
+                if 100<x<200 and 150<y<200:
+                    return"slow"
+                if 100<x<200 and 100<y<150:
+                    return"normal"
+                if 100<x<200 and 50<y<100:
+                    return"fast"
+
+        window.fill(white)
+        window.blit(game_n,(width//1-game_n.get_width() ,hight//1-game_n.get_height()))
+        window.blit(slow_b,(100,150))
+        window.blit(normal_b,(100,100))
+        window.blit(fast_b,(100,50))
+        pygame.display.update()
+
+
+
+
 
 
 def main():
-    snake=Snake()
-
+    speed_level=intro()
+    if speed_level == "slow":
+        speed_level=10
+    elif speed_level == "normal":
+        speed_level=15
+    else:
+        speed_level=20
+    snake=Snake(speed_level)
+    food=Food()
 
     clock=pygame.time.Clock()
 
@@ -64,10 +123,6 @@ def main():
                     snake.change_direction(1,0)
 
         snake.move()
-        window.fill(white)
-        for pos in snake.get_body():
-            pygame.draw.rect(window,black,(pos[0],pos[1],20,20))
-        pygame.display.update()
-        clock.tick(10)
+main()
 
 
