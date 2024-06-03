9a68b4199819323352d4c4e90df0e41bb6fc0cfd
s4bih <msabihamin@gmail.com>
2024-06-03 20:34:04 +0800

    selesai

--- counter/GUI.py
+++ counter/GUI.py
@@ -31,22 +31,14 @@ def decrease():
     global counter
     counter -= 1
     counter_label.config(text=f"counter: {counter}")
-'''
+
 def blue():
     colorname=["red","green","blue"]
     window.config(bg=random.choice(colorname))
 
 blue_button=tk.Button(window,text="change bg color",command=blue)
 blue_button.pack(side="left",pady=20,padx=20)
-'''
-counter=0
 
-if counter >= 50:
-    window.config(bg="green")
-elif counter <= -50:
-    window.config(bg="red")
-else:
-    window.config(bg="white")
 
 counter = 0
 counter_label = tk.Label(window, text=f"counter: {counter}")
